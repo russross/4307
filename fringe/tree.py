ee7654db026e3c97fe7c14b8fc2e03990e6d0a19
Russ Ross <russ@russross.com>
2026-03-02 17:15:27 -0700

    fringe with state machine

--- fringe/tree.py
+++ fringe/tree.py
@@ -41,6 +41,31 @@ def getall(node, depth=0):
     yield ('---|'*depth, node.key)
     yield from getall(node.right, depth+1)
 
+class GetAllIterator:
+    def __init__(self, node, depth=0):
+        self.stack = []
+        self.current = node
+        self.current_depth = depth
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.current is not None or self.stack:
+            while self.current is not None:
+                self.stack.append((self.current, self.current_depth))
+                self.current = self.current.left
+                self.current_depth += 1
+            self.current, self.current_depth = self.stack.pop()
+            result = ('---|'*self.current_depth, self.current.key)
+            self.current = self.current.right
+            self.current_depth += 1
+            return result
+        raise StopIteration
+
+def getall_iter(node, depth=0):
+    return GetAllIterator(node, depth)
+
 lst = list(range(1,20))
 random.shuffle(lst)
 root = None
